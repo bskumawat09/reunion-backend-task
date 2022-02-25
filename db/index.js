0c4c899025fcdd2fbc5c7397a1ab0ac6711109d7
Suresh Kumawat <bskumawat3132@gmail.com>
2022-02-25 14:16:42 +0530

    Used db connection string

--- db/index.js
+++ db/index.js
@@ -1,11 +1,9 @@
 const Pool = require("pg").Pool;
 
+const connectionString = process.env.PG_DB_STRING;
+
 const pool = new Pool({
-	user: process.env.PGUSER,
-	password: process.env.PGPASSWORD,
-	database: process.env.PGDATABASE,
-	host: process.env.PGHOST,
-	port: process.env.PGPORT,
+	connectionString
 });
 
 module.exports = pool;
