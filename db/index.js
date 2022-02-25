020d203c9eb458eaf8be7a084ec866007efced60
Suresh Kumawat <bskumawat3132@gmail.com>
2022-02-25 15:56:11 +0530

    Ssl bug

--- db/index.js
+++ db/index.js
@@ -1,7 +1,12 @@
 const Pool = require("pg").Pool;
 
-const connectionString = process.env.PG_DB_STRING;
+const connectionString = process.env.PG_DB_URL;
 
-const pool = new Pool({ connectionString });
+const pool = new Pool({
+	connectionString,
+	ssl: {
+		rejectUnauthorized: false
+	}
+});
 
 module.exports = pool;
